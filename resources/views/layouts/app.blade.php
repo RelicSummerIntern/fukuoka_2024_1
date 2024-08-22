ec15fe20e052ea63b69061a848a3b875e697dd4a
kobatakeyann <suu_hppa@outlook.jp>
2024-08-22 16:45:09 +0900

    Update title section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
 	<!-- title -->
-	<title> @yield('title')</title>
+	<title> Cook Shot </title>
 
 	<!-- Fonts -->
 	<link rel="preconnect" href="https://fonts.bunny.net">
