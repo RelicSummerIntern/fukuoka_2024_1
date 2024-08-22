481b1dc0a3c1da9e20ef71ef269c48744ecadccd
kobatakeyann <suu_hppa@outlook.jp>
2024-08-23 06:22:03 +0900

    Add footer template

---
+++ resources/views/parts/footer.blade.php
@@ -0,0 +1,40 @@
+<!-- Footer -->
+
+<div class="container-fluid bg-dark bg-gradient text-body footer mt-5 pt-5 px-0 wow fadeIn" data-wow-delay="0.1s">
+	<div class="container py-5">
+		<div class="row g-5">
+			<div class="col-lg-3 col-md-6">
+				<h3 class="text-light mb-4 text-center">Services</h3>
+				<a class="btn btn-link d-flex justify-content-center" href="">自炊をはじめる</a>
+				<a class="btn btn-link d-flex justify-content-center" href="">今日の日記を書く</a>
+				<a class="btn btn-link d-flex justify-content-center" href="">日記を見る</a>
+				<a class="btn btn-link d-flex justify-content-center" href="">他の人の投稿を見る</a>
+			</div>
+			<div class="col-lg-3 col-md-6 text-center">
+				<h3 class="text-light mb-4">Information</h3>
+				<a class="btn btn-link d-flex justify-content-center" href="">チュートリアル</a>
+				<a class="btn btn-link d-flex justify-content-center" href="">API Keyの取得</a>
+				<a class="btn btn-link d-flex justify-content-center" href="">有償プラン</a>
+				<a class="btn btn-link d-flex justify-content-center" href="">その他のサービス</a>
+			</div>
+			<div class="col-lg-3 col-md-6 text-center">
+				<h3 class="text-light mb-4">Contact</h3>
+				<a class="btn btn-link d-flex justify-content-center" href="">Support</a>
+				<a class="btn btn-link d-flex justify-content-center" href="">E-mail</a>
+				<a class="btn btn-link d-flex justify-content-center" href="">Address</a>
+				<a class="btn btn-link d-flex justify-content-center" href="">TEL & Fax</a>
+			</div>
+			<div class="col-lg-3 col-md-6">
+				<h3 class="text-light mb-4 text-center">Navigation</h3>
+				<a class="btn btn-link d-flex justify-content-center" href="#concept-img"
+					style="text-decoration: none; color:rgb(224, 148, 9);">ページ上部に戻る</a>
+				<a class="btn btn-link d-flex justify-content-center" href="#service-1"
+					style="text-decoration: none; color:rgb(224, 148, 9);">おすすめの献立の提案</a>
+				<a class="btn btn-link d-flex justify-content-center" href="#service-2"
+					style="text-decoration: none; color:rgb(224, 148, 9);">レシピの提供</a>
+				<a class="btn btn-link d-flex justify-content-center" href="#service-3"
+					style="text-decoration: none; color:rgb(224, 148, 9);">自炊日記の作成</a>
+			</div>
+		</div>
+	</div>
+</div>
