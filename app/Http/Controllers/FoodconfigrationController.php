c03c024efc8c8af4d57822096f7ce3493718454f
ykm373 <minami.ayuki379@gmail.com>
2024-08-22 10:40:25 +0900

    Add FoodconfigrationController

---
+++ app/Http/Controllers/FoodconfigrationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FoodconfigrationController extends Controller
+{
+    public function index()
+    {
+        return view('food-configration');
+    }
+}
